f778fa05344d6762f9ccd7b88cfee8b016532c04
Nicholas Padilla <nicholas@Nicholass-MacBook-Pro.local>
2018-04-12 08:14:30 -0400

    Added content file

--- popup.js
+++ popup.js
@@ -8,11 +8,35 @@ function copyToClipboard (text) {
     document.body.removeChild(sheetString);
 }
 
+function getCoordinateValue (coordinate) {
+    var inputs = document.querySelectorAll('input.position-status-coord');
+    console.log(inputs)
+    for(var input in inputs) {
+        if( input.previousSibling === coordinate) {
+            return input.value;
+        }
+    }
+    return '';
+}
+
 function getPosition () {
-    return '1\t2\t3\t'
+    var pos = {};
+    pos['x'] = getCoordinateValue('x');
+    pos['y'] = getCoordinateValue('y');
+    pos['z'] = getCoordinateValue('z');
+    return pos;
+}
+
+function getReportData (){
+    var text = '';
+    var pos = getPosition();
+    text = pos['x'].toString() + '\t'
+         + pos['y'].toString() + '\t'
+         + pos['z'].toString() + '\t';
+    copyToClipboard(text);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('getter-button').addEventListener(
-        'click', copyToClipboard('Test1'));
+        'click', getReportData)
 });
